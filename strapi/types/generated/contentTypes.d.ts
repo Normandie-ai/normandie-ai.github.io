d7145e2fb5abc2ab1d233a23fb8acb3e29ea54ce
TheoDieppois <theo76.dieppois@gmail.com>
2025-09-18 11:39:26 +0200

    feat(strapi:footer): add icon attribute to footer schema and update documentation

--- strapi/types/generated/contentTypes.d.ts
+++ strapi/types/generated/contentTypes.d.ts
@@ -372,6 +372,7 @@ export interface AdminUser extends Struct.CollectionTypeSchema {
 export interface ApiFooterFooter extends Struct.SingleTypeSchema {
   collectionName: 'footers';
   info: {
+    description: '';
     displayName: 'Footer';
     pluralName: 'footers';
     singularName: 'footer';
@@ -383,6 +384,7 @@ export interface ApiFooterFooter extends Struct.SingleTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    icon: Schema.Attribute.Media<'images' | 'files'>;
     linkedinURL: Schema.Attribute.String;
     links: Schema.Attribute.Component<'components.link', true>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
